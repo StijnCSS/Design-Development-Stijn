45a79305e6f9c0daeae280182f885017ca94de31
StijnCSS <stijnzwaard@gmail.com>
2025-02-12 14:57:54 +0100

    Covers Done
    Alle covers zijn done. Nu ga ik beginnen met animeren en styling van de pagina

---
+++ script.js
@@ -0,0 +1,6 @@
+// api documentation: https://whois.fdnd.nl/docs/
+const baseURL = 'https://fdnd.directus.app/';
+const endpointMe = 'items/person/220';
+
+const myURL = baseURL + endpointMe;
+console.log(myURL);
